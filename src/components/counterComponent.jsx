ee0c4882442adc043db2154b7f695d794a9d95ee
Chris Jose <christian.jose443@gmail.com>
2020-09-11 12:47:17 -0400

    Implemented a component that handels all counters

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   //Arrow functions dont rebinded the this keyword
   //Cleaner than adding a constructor method
-  handleIncrement = (product) => {
+  handleIncrement = () => {
     //updates the current object (must be done manually); we also pass in the attribute of state we want to modify.
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    //props in java scripts shows all attributes in counter component
+    // console.log("props", this.props);
+
     return (
       <div>
         {/*Class attributes we use bootstrap classes here */}
@@ -31,13 +33,13 @@ class Counter extends Component {
   //Uses ternary operator to change colors of button class in bootstrap
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   //Shows ZERO when counter at 0 else chow whats on the counter.
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
